8ba5bf0d29744e2f99186887af25e5ad3adb8fb0
WarlockJobs <998522+WarlockJobs@users.noreply.github.com>
2019-03-15 14:58:55 +0800

    接口的编写

---
+++ YJCommon/IDataservices.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YJCommon
+{
+    /// <summary>
+    /// 抽象类
+    /// </summary>
+    /// <typeparam name="TEntity">数据实例</typeparam>
+    /// <typeparam name="CEntity">该继承类</typeparam>
+    public abstract class IDataservices<TEntity, CEntity>
+        where TEntity : class, new()
+        where CEntity : class, new()
+    {
+        #region 单例模式
+
+        private static CEntity _instance;
+        private static object locker = new object();
+        public static CEntity GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (locker)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new CEntity();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        #endregion
+
+
+        public abstract int Create(TEntity t);
+        public abstract int Delete(int id);
+        public abstract int Update(TEntity t);
+        public abstract TEntity ShowById(int id);
+        public abstract List<TEntity> Show();
+    }
+}
---
+++ YJDAL/DaycostDAL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YJCommon;
+using YJModel;
+
+namespace YJDAL
+{
+    public class DaycostDAL : IDataservices<Daycost, DaycostDAL>
+    {
+        public override int Create(Daycost t)
+        {
+            using (DataContext db = new DataContext())
+            {
+                db.Database.CreateIfNotExists();
+                db.Daycosts.Add(t);
+                return db.SaveChanges();
+            }
+        }
+
+        public override int Delete(int id)
+        {
+            using (DataContext db = new DataContext())
+            {
+                db.Database.CreateIfNotExists();
+                db.Daycosts.Remove(db.Daycosts.Where(s => s.daycost_Id == id).FirstOrDefault());
+                return db.SaveChanges();
+            }
+        }
+
+        public override List<Daycost> Show()
+        {
+            using (DataContext db = new DataContext())
+            {
+                db.Database.CreateIfNotExists();
+                return db.Daycosts.ToList();
+            }
+        }
+
+        public override Daycost ShowById(int id)
+        {
+            using (DataContext db = new DataContext())
+            {
+                db.Database.CreateIfNotExists();
+                return db.Daycosts.ToList().Where(c => c.daycost_Id == id).FirstOrDefault();
+            }
+        }
+
+        public override int Update(Daycost t)
+        {
+            using (DataContext db = new DataContext())
+            {
+                db.Database.CreateIfNotExists();
+                db.Entry<Daycost>(t).State = System.Data.Entity.EntityState.Modified;
+                return db.SaveChanges();
+            }
+        }
+    }
+}
