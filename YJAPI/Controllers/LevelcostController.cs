9212172a6437c2a10cb99871b28534bfe6f741f2
杰大大 <1012580139@qq.com>
2019-03-15 19:13:26 +0800

    控制器

---
+++ YJAPI/Controllers/LevelcostController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YJModel;
+using YJBLL;
+using YJCommon;
+
+namespace YJAPI.Controllers
+{
+    public class LevelcostController : ApiController
+    {
+        IDataservices<Levelcost, LevelcostBLL> bll = LevelcostBLL.GetInstance();
+        [HttpPost]
+        public int Create(Levelcost levelcost)
+        {
+            return bll.Create(levelcost);
+        }
+        [HttpGet]
+        public int Delete(int id)
+        {
+            return bll.Delete(id);
+        }
+
+        [HttpGet]
+        public List<Levelcost> Show()
+        {
+            return bll.Show();
+        }
+        [HttpGet]
+        public Levelcost ShowById(int id)
+        {
+            return bll.ShowById(id);
+        }
+        [HttpPost]
+        public int Update(Levelcost levelcost)
+        {
+            return bll.Update(levelcost);
+        }
+    }
+}
---
+++ YJAPI/Controllers/SessionidsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YJModel;
+using YJBLL;
+using YJCommon;
+
+namespace YJAPI.Controllers
+{
+    public class SessionidsController : ApiController
+    {
+        IDataservices<Sessionids, SessionidsBLL> bll = SessionidsBLL.GetInstance();
+        [HttpPost]
+        public int Create(Sessionids sessionids)
+        {
+            return bll.Create(sessionids);
+        }
+        [HttpGet]
+        public int Delete(int id)
+        {
+            return bll.Delete(id);
+        }
+
+        [HttpGet]
+        public List<Sessionids> Show()
+        {
+            return bll.Show();
+        }
+        [HttpGet]
+        public Sessionids ShowById(int id)
+        {
+            return bll.ShowById(id);
+        }
+        [HttpPost]
+        public int Update(Sessionids sessionids)
+        {
+            return bll.Update(sessionids);
+        }
+    }
+}
