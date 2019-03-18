b737abdc025407213443bfd3364523184e6020fc
2941451132@qq.com <2941451132@qq.com>
2019-03-18 09:07:57 +0800

    sa

--- YJModel/Users.cs
+++ YJModel/Users.cs
@@ -15,11 +15,11 @@ namespace YJModel
         public string user_Phone { get; set; }//手机号
         public string user_Pwd { get; set; }//密码
         public string user_PhotoUrl { get; set; }//用户头像路径
-        public int user_wx_Id { get; set; }//微信号Id
-        public string user_wx_Name { get; set; }//微信名
-        public string user_wx_PhotoUrl { get; set; }//微信头像
-        public int user_wx_Sex { get; set; }//微信性别
-        public string user_wx_Country { get; set; }//微信用户国家
+        //public int user_wx_Id { get; set; }//微信号Id
+        //public string user_wx_Name { get; set; }//微信名
+        //public string user_wx_PhotoUrl { get; set; }//微信头像
+        //public int user_wx_Sex { get; set; }//微信性别
+        //public string user_wx_Country { get; set; }//微信用户国家
         public string user_Type { get; set; }//用户类型
         public float user_Backmoey { get; set; }//退回金额
     }
